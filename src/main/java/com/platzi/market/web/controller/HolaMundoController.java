ea10e4771a8ec6cb336741eedf4251025ec6f75f
pablonolasco <pablo.nolasco@live.com.mx>
2021-06-25 08:09:21 -0500

    espacio

--- src/main/java/com/platzi/market/web/controller/HolaMundoController.java
+++ src/main/java/com/platzi/market/web/controller/HolaMundoController.java
@@ -10,6 +10,6 @@ public class HolaMundoController {
 
     @GetMapping("/hola")
     public String saludar(){
-        return "Nunca pares de aprender ";
+        return "Nunca pares de aprender  ";
     }
 }
